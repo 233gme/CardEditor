39446ee3c3c81c5adbf4862b97d9230b2c90852f
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-01-20 17:31:29 +0300

    Added solutions for task1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Cart from './Cart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className='main' style={{ border: '3px solid #000', width: '100vw', height: '80vh' }}>
+        <Cart />
+      </div>
     </div>
   );
 }
