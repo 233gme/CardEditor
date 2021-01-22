e13da374426ecbffa1a4fc7983dae8e14a492ead
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-01-22 11:46:26 +0300

    Added solution for task2

--- src/Cart/index.js
+++ src/Cart/index.js
@@ -1,15 +1,32 @@
-import React from 'react'
-import './style.css'
+import React, { Component } from 'react';
+import './style.css';
 
-function Cart() {
+class Cart extends Component {
+
+  state = {
+    flag: false,
+  }
+
+  render() {
   return (
-    <div className='cart'>
+    <div className={this.state.flag === true ? 'cart cart_is_red' : 'cart cart_is_green'}>
       <div className='cart_header'>
-        Caption
+        <p>Caption</p>
+        <input type="checkbox" onClick={this.handleClick}></input>
       </div>
-      <p className='cart_text'>Text...</p>
+      <p className='cart_text'>
+        {this.props.children}
+      </p>
     </div>
   )
+  }
+
+  handleClick = () => {
+    this.setState({
+      flag: !this.state.flag
+    });
+  }
+
 }
 
 export default Cart
