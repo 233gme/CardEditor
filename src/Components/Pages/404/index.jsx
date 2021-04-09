2b974755cf1b892695a668708698545ca9c8b831
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-04-09 14:04:46 +0300

    Fixed some bugs

--- src/Components/Pages/404/index.jsx
+++ src/Components/Pages/404/index.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => (<h2 style={{ textAlign: 'center' }}>Page Not Found</h2>)
+const NotFound = () => {
+  return (
+    <Fragment>
+      <h2 style={{ textAlign: 'center' }}>Page Not Found</h2>
+      <h3 style={{ textAlign: 'center', marginTop: '25px' }}><Link to='/'>Home Page</Link></h3>
+    </Fragment>
+  )
+};
 
 export default NotFound;
